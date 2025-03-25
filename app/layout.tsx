8ea18b7c8055657a503b7941b68691ee0be1091b
Naman Saini <namansaini464@gmail.com>
2025-03-25 19:25:09 +0530

    creasated player addition logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { Inter } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const poppins = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${poppins.className} poppins-regular dark antialiased`}
       >
+        <Navbar />
+        <div className="min-h-[calc(100vh-64px)]">
         {children}
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [Players, setPlayers] = useState<string[]>([])
+  const [Current, setCurrent] = useState("")
+  const handelDelete = (index: number)=>{
+    const newPlayers = Players.filter((player, i)=>{
+      return i !== index
+    })
+    setPlayers(newPlayers)
+  }
+  const handelEdit = (index: number)=>{
+    setCurrent(Players[index])
+    handelDelete(index)
+  }
+  const addPlayers = ()=>{
+    if(Current.trim() === ""){
+      return
+    }
+    const CurrentArray = Current.split(",")
+    CurrentArray.forEach((element, index) => {
+      CurrentArray[index] = element.trim()
+    });
+    CurrentArray.forEach((element, index) => {
+      if(element === ""){
+        CurrentArray.splice(index, 1)
+      }
+    });
+    setPlayers([...Players, ...CurrentArray])
+    setCurrent("")
+  }
+  useEffect(() => {
+      const data = localStorage.getItem("Players")
+      if(data){
+        setPlayers(JSON.parse(data))
+      }
+  }, [])
+  useEffect(() => {
+      localStorage.setItem("Players", JSON.stringify(Players))
+  }, [Players])
+  
+  return (
+    <>
+    <div className="md:h-[calc(100vh-64px)] flex w-full flex-col h-[calc(100vh-200px)] poppins-regular bg-[#000] text-white">
+      {Players.length>0?  <h1 className="text-3xl p-5 font-bold">Players:</h1>: <h1 className="text-3xl p-5 font-bold">No Players</h1>}
+
+      {
+        Players.map((player, index)=>{
+          return (
+            <div key={index} className="flex w-full items-center justify-between border-b-2 border-gray-700">
+          <h1  className="text-2xl  p-5 font-bold">{player}</h1>
+          <div className="pr-5 flex justify-center items-center gap-8">
+            <button onClick={()=>handelEdit(index)}><img className="invert cursor-pointer h-6 w-6" src="/edit.svg" alt="" /></button>
+            <button onClick={()=>handelDelete(index)}><img className="invert cursor-pointer h-9 w-9" src="/delete.svg" alt="" /></button>
+          </div>
+            </div>
+        )
+        })
+      }
+
+      <div className="fixed bottom-10 w-full gap-2 justify-around border border-t-gray-500 items-center flex p-3 bg-[#000] text-white">
+
+      <div className="relative z-0 mb-5 group">
+      <input value={Current} onChange={e=>{setCurrent(e.target.value)}} type="text" name="floating_name" id="floating_name" className="block md:w-60 py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <label htmlFor="floating_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Name</label>
+
+      </div>
+      <button onClick={addPlayers} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  px-4 py-2.5 h-fit text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
+  </div>
     </div>
+    </>
   );
 }
